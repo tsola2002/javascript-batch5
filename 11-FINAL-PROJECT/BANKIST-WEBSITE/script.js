4b53e9e5aa6861a84988513378d0cdbe456fdfe7
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-11-07 12:14:39 +0100

    tabbed navigation

--- 11-FINAL-PROJECT/BANKIST-WEBSITE/script.js
+++ 11-FINAL-PROJECT/BANKIST-WEBSITE/script.js
@@ -1,4 +1,7 @@
 const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 
 ///////////////////////////////////////
@@ -44,3 +47,25 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
+
+///////////////////////////////////////
+// Tabbed component
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
